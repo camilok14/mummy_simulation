6c89d0ed0f5e7b458ea84e29371cffdbcd618fa2
Camilo Salazar <csalazarr@falabella.cl>
2019-06-30 00:27:25 -0400

    documentation for investor constructor

--- model/investor.py
+++ model/investor.py
@@ -3,6 +3,12 @@ class Investor:
     Class representing an investor from the population of potentials members of the program
     """
     def __init__(self, investor_doc):
+        """
+        Parameters
+        ----------
+        investor_doc : dict
+            Document with the attributes of the investor.
+        """
         self.innocence = investor_doc['innocence']
         self.experience = investor_doc['experience']
         self.charisma = investor_doc['charisma']
