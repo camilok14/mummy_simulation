0d24fe30e9716341f14fd6a152471f0f945f4ce8
Camilo Salazar <csalazarr@falabella.cl>
2019-06-30 17:56:14 -0400

    rest service to obtain data of the simulation from external software

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from controller.db import DatabaseController
+
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+        api = Api(self.app)        
+        api.add_resource(Health, '/health')
+        api.add_resource(MummyMoney, '/mummy_money')
+        api.add_resource(CurrentWeek, '/current_week')
+    def run(self):
+        self.app.run(port = '3030')
+
+class Health(Resource):
+    def get(self):
+        return 'Service is up and running'
+class MummyMoney(Resource):
+    def get(self):
+        db_controller = DatabaseController()
+        return db_controller.get_mummy_money()
+class CurrentWeek(Resource):
+    def get(self):
+        db_controller = DatabaseController()
+        return db_controller.get_curent_week()
+
