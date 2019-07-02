62992511ac31c7a1f099b3e926a6aae57266f6c0
Camilo Salazar <csalazarr@falabella.cl>
2019-07-02 17:15:14 -0400

    allows cross domain to rest service

--- server.py
+++ server.py
@@ -5,7 +5,13 @@ from controller.db import DatabaseController
 class Server:
     def __init__(self):
         self.app = Flask(__name__)
-        api = Api(self.app)        
+        @self.app.after_request
+        def after_request(response):
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+            response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+            return response
+        api = Api(self.app)
         api.add_resource(Health, '/health')
         api.add_resource(MummyMoney, '/mummy_money')
         api.add_resource(CurrentWeek, '/current_week')
